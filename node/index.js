caef818baef07e53da8b35e7408d0c18ee116ec5
Ednan Dias <ednan.741@gmail.com>
2023-11-04 13:32:54 -0300

    add new config on eslint node

--- node/index.js
+++ node/index.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
@@ -22,13 +23,51 @@ module.exports = {
         argsIgnorePattern: '_',
       },
     ],
+    'no-empty': [
+      'warn',
+      {
+        allowEmptyCatch: true,
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
     'prettier/prettier': [
       'warn',
       {
-        trailingComma: 'es5',
+        printWidth: 80,
         tabWidth: 2,
         semi: true,
         singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        endOfLine: 'lf',
       },
     ],
   },
